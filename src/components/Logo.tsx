d0a6f4c3302fe7b245b8e2caec8dce2869f034ff
Adrian Drzazgowski <iamdrzazgowski@gmail.com>
2025-09-01 17:02:21 +0200

    signup page design

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,8 +3,8 @@ import { IoChatbubbleEllipsesOutline } from 'react-icons/io5';
 export default function Logo() {
     return (
         <div className='flex gap-2 items-center justify-center'>
-            <IoChatbubbleEllipsesOutline className='w-12 h-12 ' />
-            <span className='text-3xl font-bold'>Chat</span>
+            <IoChatbubbleEllipsesOutline className='w-12 h-12 text-blue-600' />
+            <span className='text-3xl font-bold text-gray-800'>Chat</span>
         </div>
     );
 }
