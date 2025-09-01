132391c3bf0199906e99f950162fc21dfb876530
Adrian Drzazgowski <iamdrzazgowski@gmail.com>
2025-09-01 16:20:36 +0200

    signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import AppLayout from './components/AppLayout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Signup from './pages/Signup';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 0,
+        },
+    },
+});
+
 function App() {
-    return <h1>TEST</h1>;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Signup />} />
+                    {/* <Route element={<AppLayout />}></Route> */}
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
