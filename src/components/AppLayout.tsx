2697a397e586232fcd4d67f35d23a00b94c375bf
Adrian Drzazgowski <iamdrzazgowski@gmail.com>
2025-09-03 15:50:44 +0200

    sidebar design

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -3,10 +3,15 @@ import Sidebar from './Sidebar';
 
 export default function AppLayout() {
     return (
-        <div>
-            <h1>XDDDDDDDDDDDDDd</h1>
-            <Sidebar />
-            <Outlet />
+        <div className='flex justify-center items-center h-screen bg-gray-900'>
+            <div className='flex w-3/4 h-3/4 shadow-lg rounded-2xl overflow-hidden'>
+                <aside className='w-1/4 bg-gray-200 h-full'>
+                    <Sidebar />
+                </aside>
+                <main className='w-3/4 bg-white flex flex-col h-full'>
+                    <Outlet />
+                </main>
+            </div>
         </div>
     );
 }
